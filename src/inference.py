0afd1e801259dc06f17430ecb0094889ae0956c5
Szymon Trochimiak <szymon.trochimiak@nomagic.ai>
2025-01-06 16:11:59 +0100

    Update inference script

--- src/inference.py
+++ src/inference.py
@@ -1,4 +1,7 @@
 import torch
+import os
+
+from PIL.ImageOps import scale
 
 from model.scaler import Scaler
 from model.model import FlightNN
@@ -6,7 +9,25 @@ import numpy as np
 from gcp.load_file import GCSLoader
 
 
-def inference(scaler_x: Scaler, scaler_y: Scaler, model: FlightNN, X: np.ndarray) -> float:
+def inference(X: np.ndarray) -> float:
+    loader = GCSLoader(bucket_name="models-big-data-mini")
+    best_model = "best_model.pth"
+    if not os.path.isfile(best_model):
+        loader.load_to_path("best_model.pth", best_model)
+
+    model = FlightNN(input_size=2)
+    model.load(best_model)
+
+    scaler_x_path = "scaler_X.json"
+    if not os.path.isfile(scaler_x_path):
+        loader.load_to_path("scaler_X.json", scaler_x_path)
+    scaler_x = Scaler.load(scaler_x_path)
+
+    scaler_y = "scaler_y.json"
+    if not os.path.isfile(scaler_y):
+        loader.load_to_path("scaler_y.json", scaler_y)
+    scaler_y = Scaler.load(scaler_y)
+
     assert X.shape[1] == 2, f"Expected 2 features, got {X.shape[1]}"
 
     X_scaled = scaler_x.transform(X)
@@ -16,16 +37,7 @@ def inference(scaler_x: Scaler, scaler_y: Scaler, model: FlightNN, X: np.ndarray
 
 
 if __name__ == "__main__":
-    # Load model and scalers
-    loader = GCSLoader(bucket_name="models-big-data-mini")
-
-    model = FlightNN(input_size=2)
-    model.load(loader.load_to_memory("best_model.pth"))
-
-    scaler_x = Scaler.load(loader.load_to_temp("scaler_X.json"))
-    scaler_y = Scaler.load(loader.load_to_temp("scaler_y.json"))
-
     # Inference
     X = np.array([[1200, 500]])
-    y_pred = inference(scaler_x, scaler_y, model, X)
+    y_pred = inference(X)
     print(f"Predicted delay: {y_pred:.2f} seconds")
